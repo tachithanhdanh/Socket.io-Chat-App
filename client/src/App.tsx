d93401122161b23fb4ad25f8d7f4c5cf04e6f466
Ta Chi Thanh Danh <22120049@student.hcmus.edu.vn>
2024-08-09 15:25:31 +0700

    Add sample files

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+// Create by `rafce` command
+import React from 'react';
+
+const App = () => {
+  return (
+    <h1>Hello World</h1>
+  )
+};
+
+export default App;
---
+++ client/src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { BrowserRouter } from 'react-router-dom';
+
+// Render the app through add a root element to the DOM
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  // Wrap the app with the BrowserRouter to enable routing
+  // BrowserRouter is a component that provides the routing functionality to the app
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
